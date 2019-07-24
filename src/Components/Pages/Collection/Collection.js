34d73c53a3981c76a50d975a1823c950d9a45506
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-23 20:59:19 -0500

    have carts printing, but all carts, not by user ID

--- src/Components/Pages/Collection/Collection.js
+++ src/Components/Pages/Collection/Collection.js
@@ -1,11 +1,83 @@
 import React from 'react';
+import SearchField from 'react-search-field';
+import PrintCollectionCard from '../../PrintCollectionCard/PrintCollectionCard';
+import collectionRequests from '../../../helpers/Data/collectionRequests';
+import userRequests from '../../../helpers/Data/userRequests';
+import authRequests from '../../../helpers/Data/authRequests';
 import './Collection.scss';
 
 class Collection extends React.Component {
+  state = {
+    myCarts: [],
+    filteredMyCarts: [],
+    currentUserObj: {},
+  };
+
+  getCollection = () => {
+    collectionRequests
+      .getAllMyCarts()
+      .then((myCarts) => {
+        this.setState({ myCarts });
+        this.setState({ filteredMyCarts: myCarts });
+      })
+      .catch((err) => {
+        console.error('error with NESVault Collection GET', err);
+      });
+  };
+
+  componentDidMount() {
+    const currentUid = authRequests.getCurrentUid();
+    this.getCollection();
+    userRequests.getUserByFbId(currentUid).then((response) => {
+      this.setState({
+        currentUserObj: response,
+      });
+    });
+  }
+
+  onChange = (value, event) => {
+    const { myCarts } = this.state;
+    const filteredMyCarts = [];
+    event.preventDefault();
+    if (!value) {
+      this.setState({ filteredMyCarts: myCarts });
+    } else {
+      myCarts.forEach((myCart) => {
+        if (
+          myCart.name.toLowerCase().includes(value.toLowerCase())
+          || myCart.description.toLowerCase().includes(value.toLowerCase())
+        ) {
+          filteredMyCarts.push(myCart);
+        }
+        this.setState({ filteredMyCarts });
+      });
+    }
+  };
+
+  onSelect = (id) => {
+    this.props.history.push(`/mycarts/${id}`);
+  };
+
   render() {
+    const { filteredMyCarts } = this.state;
+
+    const printCollection = filteredMyCarts.map(myCart => (
+      <PrintCollectionCard key={myCart.id} myCart={myCart} onSelect={this.onSelect} />
+    ));
+
     return (
-      <div>
-        <h1>You're Gonna Need A Bigger Shelf</h1>
+      <div className="myCarts mx-auto animated fadeIn w-100">
+        <div className="myCartsWrap">
+          <SearchField
+            placeholder="Search Collection By Name or Genre"
+            onChange={this.onChange}
+            searchText=""
+            classNames="collectionSearch"
+          />
+        </div>
+        <div className="myCartWindow">
+          <div className="row justify-content-center">{printCollection}</div>
+        </div>
       </div>
     );
   }
---
+++ src/helpers/Data/collectionRequests.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import firebase from 'firebase/app';
+import apiKeys from '../apiKeys';
+
+const nesVaultApiBaseUrl = apiKeys.nesVaultApi.apiBaseUrl;
+
+const getCurrentUid = () => firebase.auth().currentUser.uid;
+
+// Get All MyCarts For Collection Page //
+const getAllMyCarts = () => new Promise((resolve, reject) => {
+  axios
+    .get(`${nesVaultApiBaseUrl}/api/mycarts/`)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => reject(error));
+});
+
+// Get Single MyCart By ID //
+const getSingleMyCart = id => new Promise((resolve, reject) => {
+  axios
+    .get(`${nesVaultApiBaseUrl}/api/mycarts/${id}`)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => reject(error));
+});
+
+// Get Single MyCart By Click //
+const getSingleMyCartClick = id => new Promise((resolve, reject) => {
+  axios
+    .get(`${nesVaultApiBaseUrl}/api/mycarts/${id}`)
+    .then((result) => {
+      const singleItem = result.data;
+      singleItem.id = id;
+      resolve(singleItem);
+    })
+    .catch(error => reject(error));
+});
+
+// Create MyCart Call //
+const createMyCart = newMyCart => new Promise((resolve, reject) => {
+  axios
+    .post(`${nesVaultApiBaseUrl}/api/mycart/`, newMyCart)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => reject(error));
+});
+
+// Edit MyCart Call //
+const editMyCart = (id, myCart) => new Promise((resolve, reject) => {
+  axios
+    .put(`${nesVaultApiBaseUrl}/api/mycarts/update/${id}`, myCart)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => reject(error));
+});
+
+const deleteMyCart = myCartId => axios.delete(`${nesVaultApiBaseUrl}/api/mycarts/delete/${myCartId}`);
+
+export default {
+  getAllMyCarts,
+  getSingleMyCart,
+  createMyCart,
+  editMyCart,
+  deleteMyCart,
+  getCurrentUid,
+  getSingleMyCartClick,
+};
