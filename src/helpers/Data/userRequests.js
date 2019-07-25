1962324b1ff62e2c8424df0e4b1fc06a0b12330f
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-24 23:14:56 -0500

    built register form, can't get to pop up

--- src/helpers/Data/userRequests.js
+++ src/helpers/Data/userRequests.js
@@ -12,6 +12,36 @@ const getUserByFbId = uid => new Promise((resolve, reject) => {
     .catch(error => reject(error));
 });
 
+const createUser = newUser => new Promise((resolve, reject) => {
+  axios
+    .post(`${nesVaultApiBaseUrl}/api/Users/`, newUser)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => console.error('Error creating user', error));
+});
+
+const updateUser = newUser => new Promise((resolve, reject) => {
+  axios
+    .put(`${nesVaultApiBaseUrl}/api/Users/${newUser.id}`, newUser)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => console.error('Error updating user', error));
+});
+
+const deleteUser = userId => new Promise((resolve, reject) => {
+  axios
+    .delete(`${nesVaultApiBaseUrl}/api/Users/${userId}`)
+    .then((result) => {
+      resolve(result.data);
+    })
+    .catch(error => console.error('Error deleting user'));
+});
+
 export default {
   getUserByFbId,
+  createUser,
+  updateUser,
+  deleteUser,
 };
