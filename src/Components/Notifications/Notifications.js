cc410f8b58dc87ea4dd3c1613b6619af751667f0
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-08-03 00:33:01 -0500

    built custom alert for cartlist buttons, working, but need to tweak

---
+++ src/Components/Notifications/Notifications.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+import ee from 'event-emitter';
+
+import './Notifications.scss';
+
+const Container = styled.div`
+  background-color: black;
+  color: red;
+  padding: 16px;
+  position: absolute;
+  border-radius: 15px;
+  border: 2px solid white;
+  top: ${props => props.top}px;
+  right: 16px;
+  z-index: 666;
+  transition: top 0.5s ease;
+`;
+
+const emitter = new ee();
+
+export const notify = (msg) => {
+  emitter.emit('notification', msg);
+};
+
+class Notifications extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      top: -100,
+      msg: '',
+    };
+
+    this.timeout = null;
+
+    emitter.on('notification', (msg) => {
+      this.onShow(msg);
+    });
+  }
+
+  onShow = (msg) => {
+    if (this.timout) {
+      clearTimeout(this.timeout);
+      this.setState({ top: -100 }, () => {
+        this.timeout = setTimeout(() => {
+          this.showNotification();
+        }, 500);
+      });
+    } else {
+      this.showNotification(msg);
+    }
+  };
+
+  showNotification = () => {
+    this.setState(
+      {
+        top: 16,
+        msg: '',
+      },
+      () => {
+        this.timeout = setTimeout(() => {
+          this.setState({
+            top: -100,
+          });
+        }, 3000);
+      },
+    );
+  };
+
+  render() {
+    return (
+      <Container className="test-container" top={this.state.top}>
+        <i className="game-added fas fa-thumbs-up fa-2x" />
+      </Container>
+    );
+  }
+}
+
+export default Notifications;
