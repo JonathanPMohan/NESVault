b503c00f7c65fb1f935efdcb862feaf44f72e179
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-08-03 15:18:49 -0500

    wishlist cart delete working

--- src/Components/PrintWishListCard/PrintWishListCard.js
+++ src/Components/PrintWishListCard/PrintWishListCard.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Utility from '../../helpers/utils/Utility';
 
 import './PrintWishListCard.scss';
 
 class PrintWishListCard extends React.Component {
-  static propTypes = {
-    deleteSingleWishListItem: PropTypes.func,
-    // userObject: PropTypes.object,
-  };
-
   myWishListClick = () => {
     const { wishList, onSelect } = this.props;
     onSelect(wishList.id);
   };
 
+  deleteWishListCart = (e) => {
+    e.preventDefault();
+    const { deleteMyWishListCart, wishList } = this.props;
+    deleteMyWishListCart(wishList.id);
+  };
+
   render() {
-    const { myCart, wishList } = this.props;
+    const { wishList } = this.props;
 
     return (
       <div className="myCartCard col-2">
@@ -25,13 +25,13 @@ class PrintWishListCard extends React.Component {
         <h6 className="myCart-card-header">{wishList.genre}</h6>
         <h6 className="myCart-card-header">{Utility.dateFormat(wishList.releaseDate)}</h6>
         <h6 className="collection-loose-price">${wishList.loose}</h6>
-        <div className="cart-card-body" onClick={this.myWishListClick}>
+        <div className="cart-card-body">
           <h5 className="card-text">
             <button className="myCart-details" onClick={this.myWishListClick}>
               INFO
               <span className="lnr lnr-list cart-info-icon" />
             </button>
-            <button className="delete-cart-button" onClick={this.deleteCart}>
+            <button className="delete-cart-button" id={wishList.id} onClick={this.deleteWishListCart}>
               <span className="lnr lnr-cross-circle delete-cart-icon" />
             </button>
           </h5>
